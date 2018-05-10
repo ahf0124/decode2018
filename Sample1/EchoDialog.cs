a22d65315a97833a0d4cee0b694ecf525e2c785d
Ahf <mail_ahf@yahoo.co.jp>
2018-05-10 14:17:02 +0900

    Update EchoDialog.cs
    アクティビティをそのまま連携するように変更

--- Sample1/EchoDialog.cs
+++ Sample1/EchoDialog.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
@@ -34,18 +35,21 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                var logicappsUrl = "[Logic Apps で生成された URL]";
+                //var logicappsUrl = "[Logic Apps で生成された URL]";
+                var logicappsUrl = "https://prod-31.westcentralus.logic.azure.com:443/workflows/c73c2cf351864f58a28dc4d33b70ba15/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=4oH6NWgNaIZtuet9od1b9KxKu9dhS2V3AenUN9f2B9w";
                 //メッセージを編集する Logic Apps を呼び出す
-                var msg = message.Text;
+                var msg = "";
                 var client = new HttpClient();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var content =  new StringContent("{\"message\":\"" + msg + "\"}", System.Text.Encoding.UTF8, "application/json");
+                var content =  new StringContent(JsonConvert.SerializeObject(message), System.Text.Encoding.UTF8, "application/json");
                 //Logic Apps の HTTP Request コネクタで生成される URL を指定して呼び出す
                 var response = await client.PostAsync(logicappsUrl,content);
                  if (response.IsSuccessStatusCode)
                  {
                      //呼出が成功した場合、結果を文字列として取得する
                       msg = await response.Content.ReadAsStringAsync();
+                 }else{
+                     msg = message.Text;
                  }
                  //結果を返却する
                 await context.PostAsync($"{this.count++}: You said {msg}");
